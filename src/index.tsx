a1794fde514b99a9b52766f26584c334eb9dbe27
Alexa357 <aleksa.petrovic1998@gmail.com>
2023-12-03 21:21:00 +0100

    podesavanje nswag

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Client } from "./generated/iop";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+let client = new Client();
+// client
+//   .napraviKorisnika(
+//     new Korisnik({
+//       id: 0,
+//       jmbg: "555",
+//       ime: "onaj",
+//       prezime: "sto sam te jebo",
+//       korisnickoIme: "onaj",
+//       lozinka: "onajstosamtejebo",
+//     })
+//   )
+//   .then((x) => console.log(x));
+let korisnici = client.dajSveKorisnike().then((x) => console.log(x));
 root.render(
   <React.StrictMode>
     <App />
